fb6e43472f1163586cc96c82638c7d3c7aa3c70e
ddorian <dorian.hoxha@gmail.com>
2024-05-01 10:14:17 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+import gevent.monkey
+
+gevent.monkey.patch_all()
+
+import logfire
+
+logfire.configure(token="")
+# todo doing this import shows the error
+import magic
+from flask import Flask
 
 app = Flask(__name__)
+
+
+from flask import render_template
+
 
 @app.route("/")
 def hello_world():
